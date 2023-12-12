4b056055782c318e5d96bd6f1adbb98eedfd3dbf
Tien Nguyen <tien.nguyen@asnet.com.vn>
2023-12-12 11:23:52 +0700

    Implement Logo component

---
+++ practice-four/src/components/Logo/index.tsx
@@ -0,0 +1,30 @@
+import { Box, Flex, Image, Text } from '@chakra-ui/react';
+
+const Logo = () => (
+  <Flex gap='2.5'>
+    <Image src='technician.svg' alt='Technician' />
+    <Box>
+      <Text
+        width='fit-content'
+        color='white'
+        fontWeight='normal'
+        variant='textLg'
+        letterSpacing='0.275px'
+      >
+        Technician
+      </Text>
+      <Text
+        width='fit-content'
+        fontWeight='normal'
+        variant='textSm'
+        color='white'
+        opacity='0.5'
+        letterSpacing='0.193px'
+      >
+        At Home
+      </Text>
+    </Box>
+  </Flex>
+);
+
+export default Logo;
