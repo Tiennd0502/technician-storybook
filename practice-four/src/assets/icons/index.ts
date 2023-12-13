bcd6507afb4c834a4517c18b01d9e3e7c13b5164
Tien Nguyen <tien.nguyen@asnet.com.vn>
2023-12-13 15:49:11 +0700

    Add icons for practice

--- practice-four/src/assets/icons/index.ts
+++ practice-four/src/assets/icons/index.ts
@@ -1,1 +1,8 @@
-export { default as NotificationIcon } from './notificate-icon';
+export { default as NotificationIcon } from './NotificateIcon';
+export { default as DashboardIcon } from './DashboardIcon';
+export { default as FeedbackIcon } from './FeedbackIcon';
+export { default as HistoryIcon } from './HistoryIcon';
+export { default as ReportIcon } from './ReportIcon';
+export { default as ServiceIcon } from './ServiceIcon';
+export { default as TechnicianIcon } from './TechnicianIcon';
+export { default as ArrowLeftIcon } from './ArrowLeftIcon';
