a877aa3ac15d6f5eb13215442f366dbfa877770b
Tien Nguyen <tien.nguyen@asnet.com.vn>
2023-12-14 09:44:46 +0700

    Update style for Header,Sidebar, Technician component

--- practice-four/src/components/Technician/index.tsx
+++ practice-four/src/components/Technician/index.tsx
@@ -1,7 +1,14 @@
 import { Box, Heading, Text, Image } from '@chakra-ui/react';
 
 const Technician = () => (
-  <Box p='7' borderWidth='1px' borderColor='border.primary' borderRadius='md' w='fit-content'>
+  <Box
+    p='7'
+    borderWidth='1px'
+    borderColor='border.primary'
+    borderRadius='md'
+    w='fit-content'
+    height='100%'
+  >
     <Heading variant='headingXl'>Add Technicians</Heading>
     <Text as='p' variant='textXxs'>
       Click on the &nbsp;
